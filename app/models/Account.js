6a35bfe04e89a12da08a8b2267b5447e016e1871
Carlos <carlosdelros@gmail.com>
2018-01-29 21:56:11 -0500

    hooks up models to app adds storage path for db in config.json finish model

--- app/models/Account.js
+++ app/models/Account.js
@@ -1,5 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
-    const Account = sequelize.define('account', {
-        
-    })
-}
+  const Account = sequelize.define('account', {
+    name: {
+      type: DataTypes.STRING
+    },
+    onBudget: {
+      type: DataTypes.BOOLEAN
+    },
+    accountType: {
+      type: DataTypes.INTEGER
+    },
+    hidden: {
+      type: DataTypes.BOOLEAN
+    },
+    lastReconciledDate: {
+      type: DataTypes.DATE
+    }
+  })
+  return Account;
+};
